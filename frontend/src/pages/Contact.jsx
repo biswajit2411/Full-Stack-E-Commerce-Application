8ad807b721f323b97318163ded58a1f315719e06
biswajit2411 <biswajit59389@gmail.com>
2025-11-26 13:39:34 +0530

    first commit

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox'
+
+const Contact = () => {
+  return (
+    <div>
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-700'>Our Store</p>
+          <p className='text-gray-600'>Girish Chandra Road <br /> kolkata-700009</p>
+          <p className='text-gray-600'>Tel: (+91) 000-0088 <br /> Email: admin@forever.com</p>
+          <p className='font-semibold text-xl text-gray-700'>Careers at Forever</p>
+          <p className='text-gray-600'>Learn more about teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+      <NewsLetterBox/>
+    </div>
+  )
+}
+
+export default Contact
